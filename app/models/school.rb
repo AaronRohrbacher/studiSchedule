7e77c1d26d0be4fed9d681eb896be01ec2d7f6a3
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-15 19:00:21 -0800

    create initial school model

---
+++ app/models/school.rb
@@ -0,0 +1,11 @@
+class School < ApplicationRecord
+  has_many :accounts
+  has_many :teachers
+  has_many :timereports
+  has_many :attendances
+  has_many :teacherEvents
+  has_many :accountevents
+  has_many :events
+  has_many :times
+  has_many :rooms
+end
