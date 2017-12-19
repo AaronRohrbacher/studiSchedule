b7b6b46474892bd1b972f5eb1191e7a5c21af6d9
Aaron Rohrbacher <arjazz@icloud.com>
2017-12-19 15:13:24 -0800

    add schedule and room to an event

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
   def new
     @school = School.find(params[:school_id])
+    @rooms = Room.all
     @event = Event.find(params[:event_id])
     @schedule = Schedule.new
   end
@@ -9,6 +10,6 @@ class SchedulesController < ApplicationController
     @school = School.find(params[:school_id])
     @event = Event.find(params[:event_id])
 
-    Schedule.create!(school_id: @school.id, event_id: @event.id)
+    Schedule.create!(school_id: @school.id, event_id: @event.id, room_id: params[:schedule][:room_id], day: params[:schedule][:day], start_time: params[:schedule][:start_time], end_time: params[:schedule][:end_time])
   end
 end
