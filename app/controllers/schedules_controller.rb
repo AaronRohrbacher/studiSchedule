e8f415b2b2a86e0b86536bbbb9938bfaed9d5638
Aaron Rohrbacher <arjazz@icloud.com>
2018-02-18 13:08:40 -0800

    add recurring scheduling options

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,24 +10,35 @@ class SchedulesController < ApplicationController
   def create
     end_of_semester = Time.utc(2018, 3, 30)
     @school = School.find(params[:school_id])
-    @event = Event.find(params[:event_id])
-    start_time = Time.utc(2000,01,01,params[:schedule]['start_time(4i)'].to_i, params[:schedule]['start_time(5i)'].to_i)
-    end_time = Time.utc(2000,01,01,params[:schedule]['end_time(4i)'].to_i, params[:schedule]['end_time(5i)'].to_i)
-
-    # start_time = params[:schedule][:start_time].to_time
-    # end_time = params[:schedule][:end_time].to_time
-
+    @event = @school.events.find(params[:event_id])
+    start_time = Time.utc(2000,01,01,params[:schedule]['start_time(4i)'].to_i,
+                 params[:schedule]['start_time(5i)'].to_i)
+    end_time = Time.utc(2000,01,01,params[:schedule]['end_time(4i)'].to_i,
+                 params[:schedule]['end_time(5i)'].to_i)
 
     if Room.has_scheduling_conflict?(params[:schedule][:room_id], start_time, end_time)
       flash[:alert] = "Schedule Conflict"
     else
       recurring_date = Date.parse(params[:schedule][:date])
       if params[:schedule][:recurring] == 'semester'
-        until recurring_date >= end_of_semester do
-          Schedule.create!(school_id: @school.id, event_id: @event.id, room_id: params[:schedule][:room_id], date: recurring_date, start_time: start_time, end_time: end_time, date: recurring_date)
+        end_recurring_date = end_of_semester
+      elsif params[:schedule][:recurring] == 'once'
+        end_recurring_date = recurring_date
+      elsif params[:schedule][:recurring] = 'indefinite'
+        end_recurring_date = recurring_date + 1.year
+      end
+      
+        until recurring_date > end_recurring_date do
+          Schedule.create!(
+            school_id: @school.id,
+            event_id: @event.id,
+            room_id: params[:schedule][:room_id],
+            date: recurring_date,
+            start_time: start_time,
+            end_time: end_time,
+            date: recurring_date)
           recurring_date = recurring_date + 1.week
         end
-      end
     end
   end
 
