e9d907407ca1cb90af299d4b92d7a59eb6751452
Aaron Rohrbacher <arjazz@icloud.com>
2018-05-16 18:40:12 -0700

    set timezone to pacific for schedule viewing

--- app/controllers/full_schedules_controller.rb
+++ app/controllers/full_schedules_controller.rb
@@ -2,9 +2,9 @@ class FullSchedulesController < ApplicationController
   def index
     @school = School.find(params[:school_id])
     if params[:start_date]
-      @html = Schedule.display_schedule(params[:start_date][0].to_date, @school.id)
+      @html = Schedule.display_schedule(params[:start_date][0].to_date.in_time_zone("Pacific Time (US & Canada)"), @school.id)
     else
-      @html = Schedule.display_schedule(Time.now, @school.id)
+      @html = Schedule.display_schedule(Time.now.in_time_zone("Pacific Time (US & Canada)"), @school.id)
     end
     params.delete :start_date
   end
